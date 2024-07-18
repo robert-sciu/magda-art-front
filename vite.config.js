1f0a44753b790e2fec6ec933ab7e781a1f42cc1f
robert-sciu <robert.sciu@gmail.com>
2024-07-18 09:25:42 +0200

    added csp into build config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import viteCompression from "vite-plugin-compression";
 import legacy from "@vitejs/plugin-legacy";
 import { visualizer } from "rollup-plugin-visualizer";
 import Sitemap from "vite-plugin-sitemap";
-// import csp from "vite-plugin-csp";
+import csp from "vite-plugin-csp";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,28 +21,28 @@ export default defineConfig({
     //   filename: "./dist/stats.html",
     //   open: true,
     // }),
-    // csp({
-    //   policies: {
-    //     "default-src": ["'self'"],
-    //     "script-src": ["'report-sample'", "'self'"],
-    //     "style-src": ["'report-sample'", "'self'"],
-    //     "object-src": ["'none'"],
-    //     "base-uri": ["'self'"],
-    //     "connect-src": ["'self'", "https://magda-art.click"],
-    //     "font-src": ["'self'"],
-    //     "frame-src": ["'self'"],
-    //     "img-src": [
-    //       "'self'",
-    //       "https://robert-sciu-magda-art-bucket.s3.eu-central-1.amazonaws.com",
-    //     ],
-    //     "manifest-src": ["'self'"],
-    //     "media-src": ["'self'"],
-    //     "report-uri": [
-    //       "https://6697fff2b594446855d88658.endpoint.csper.io/?v=0",
-    //     ],
-    //     "worker-src": ["'none'"],
-    //   },
-    // }),
+    csp({
+      policies: {
+        "default-src": ["'self'"],
+        "script-src": ["'report-sample'", "'self'"],
+        "style-src": ["'report-sample'", "'self'"],
+        "object-src": ["'none'"],
+        "base-uri": ["'self'"],
+        "connect-src": ["'self'", "https://magda-art.click"],
+        "font-src": ["'self'"],
+        "frame-src": ["'self'"],
+        "img-src": [
+          "'self'",
+          "https://robert-sciu-magda-art-bucket.s3.eu-central-1.amazonaws.com",
+        ],
+        "manifest-src": ["'self'"],
+        "media-src": ["'self'"],
+        // "report-uri": [
+        //   "https://6697fff2b594446855d88658.endpoint.csper.io/?v=0",
+        // ],
+        "worker-src": ["'none'"],
+      },
+    }),
     // createHtmlPlugin({
     //   inject: {
     //     injectData: {
