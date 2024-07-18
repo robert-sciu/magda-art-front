a12666c81f3d4b7b1942b8ef7eecd83a9de5abbb
robert-sciu <robert.sciu@gmail.com>
2024-07-18 09:01:18 +0200

    changed spinners

--- src/components/common/spinner/Spinner.jsx
+++ src/components/common/spinner/Spinner.jsx
@@ -2,13 +2,26 @@ import PropTypes from "prop-types";
 
 import scss from "../../../../styles/variables.module.scss";
 
-import { SpinnerDotted } from "spinners-react";
+// import { SpinnerDotted } from "spinners-react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+// export default function Spinner({
+//   size = scss.sizeXxxxl,
+//   color = scss.mainLightGray,
+// }) {
+//   return <SpinnerDotted size={size} color={color} />;
+// }
+
+// Spinner.propTypes = {
+//   size: PropTypes.string,
+//   color: PropTypes.string,
+// };
 
 export default function Spinner({
   size = scss.sizeXxxxl,
   color = scss.mainLightGray,
 }) {
-  return <SpinnerDotted size={size} color={color} />;
+  return <ClipLoader size={size} color={color} />;
 }
 
 Spinner.propTypes = {
