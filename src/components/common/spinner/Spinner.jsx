65a0cbdf6cfa252c08ceabae32d7542521be3e1a
robert-sciu <robert.sciu@gmail.com>
2024-07-18 15:53:02 +0200

    added very simple spinner

--- src/components/common/spinner/Spinner.jsx
+++ src/components/common/spinner/Spinner.jsx
@@ -1,9 +1,12 @@
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 import scss from "../../../../styles/variables.module.scss";
 
 // import { SpinnerDotted } from "spinners-react";
-import ClipLoader from "react-spinners/ClipLoader";
+// import ClipLoader from "react-spinners/ClipLoader";
+import spinner from "../../../assets/svg-spinners--eclipse.svg";
+
+import styles from "./spinner.module.scss";
 
 // export default function Spinner({
 //   size = scss.sizeXxxxl,
@@ -30,5 +33,5 @@ import ClipLoader from "react-spinners/ClipLoader";
 // };
 
 export default function Spinner() {
-  return <p>.</p>;
+  return <img src={spinner} alt="spinner" className={styles.spinner} />;
 }
