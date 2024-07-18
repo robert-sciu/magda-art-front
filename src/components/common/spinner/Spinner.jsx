7d7c26640f2a2add94628ed8d02222e6be37c1b4
robert-sciu <robert.sciu@gmail.com>
2024-07-18 16:11:29 +0200

    checking out spinner

--- src/components/common/spinner/Spinner.jsx
+++ src/components/common/spinner/Spinner.jsx
@@ -2,37 +2,27 @@ import PropTypes from "prop-types";
 
 import scss from "../../../../styles/variables.module.scss";
 
-// import { SpinnerDotted } from "spinners-react";
+import { SpinnerDotted } from "spinners-react";
 import ClipLoader from "react-spinners/ClipLoader";
 
-// import spinner from "../../../assets/svg-spinners--eclipse.svg";
-import spinner from "../../../assets/spinner.svg";
+import spinner from "../../../assets/svg-spinners--eclipse.svg";
+// import spinner from "../../../assets/spinner.svg";
 
 import styles from "./spinner.module.scss";
-
-// export default function Spinner({
-//   size = scss.sizeXxxxl,
-//   color = scss.mainLightGray,
-// }) {
-//   return <SpinnerDotted size={size} color={color} />;
-// }
-
-// Spinner.propTypes = {
-//   size: PropTypes.string,
-//   color: PropTypes.string,
-// };
 
 export default function Spinner({
   size = scss.sizeXxxxl,
   color = scss.mainLightGray,
 }) {
-  return <ClipLoader size={size} color={color} />;
+  return <SpinnerDotted size={size} color={color} />;
 }
 
-Spinner.propTypes = {
-  size: PropTypes.string,
-  color: PropTypes.string,
-};
+// export default function Spinner({
+//   size = scss.sizeXxxxl,
+//   color = scss.mainLightGray,
+// }) {
+//   return <ClipLoader size={size} color={color} />;
+// }
 
 // export default function Spinner({
 //   size = scss.sizeXxxxl,
@@ -41,7 +31,7 @@ Spinner.propTypes = {
 //   return <img src={spinner} alt="spinner" className={styles.spinner} />;
 // }
 
-// Spinner.propTypes = {
-//   size: PropTypes.string,
-//   color: PropTypes.string,
-// };
+Spinner.propTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string,
+};
