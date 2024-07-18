f40ab6b0424daa45f63c9a9d4a3ad54659df5bea
robert-sciu <robert.sciu@gmail.com>
2024-07-18 16:05:33 +0200

    checking out spinner

--- src/components/common/spinner/Spinner.jsx
+++ src/components/common/spinner/Spinner.jsx
@@ -1,10 +1,12 @@
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
 import scss from "../../../../styles/variables.module.scss";
 
 // import { SpinnerDotted } from "spinners-react";
-// import ClipLoader from "react-spinners/ClipLoader";
-import spinner from "../../../assets/svg-spinners--eclipse.svg";
+import ClipLoader from "react-spinners/ClipLoader";
+
+// import spinner from "../../../assets/svg-spinners--eclipse.svg";
+import spinner from "../../../assets/spinner.svg";
 
 import styles from "./spinner.module.scss";
 
@@ -20,18 +22,26 @@ import styles from "./spinner.module.scss";
 //   color: PropTypes.string,
 // };
 
+export default function Spinner({
+  size = scss.sizeXxxxl,
+  color = scss.mainLightGray,
+}) {
+  return <ClipLoader size={size} color={color} />;
+}
+
+Spinner.propTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string,
+};
+
 // export default function Spinner({
 //   size = scss.sizeXxxxl,
 //   color = scss.mainLightGray,
 // }) {
-//   return <ClipLoader size={size} color={color} />;
+//   return <img src={spinner} alt="spinner" className={styles.spinner} />;
 // }
 
 // Spinner.propTypes = {
 //   size: PropTypes.string,
 //   color: PropTypes.string,
 // };
-
-export default function Spinner() {
-  return <img src={spinner} alt="spinner" className={styles.spinner} />;
-}
