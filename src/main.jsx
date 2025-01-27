ddad6214ea6f69ad1c51d1fb45c1637f394dc194
robert-sciu <robert.sciu@gmail.com>
2025-01-27 07:56:50 +0100

    disabled stricts

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,10 @@ import LoadingState from "./components/loadingState/loadingState.jsx";
 import { selectAppLoaded } from "./store/rootNavSlice.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-      <LoadingState appLoadedSelector={selectAppLoaded} />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+    <LoadingState appLoadedSelector={selectAppLoaded} />
+  </Provider>
+  // </React.StrictMode>
 );
